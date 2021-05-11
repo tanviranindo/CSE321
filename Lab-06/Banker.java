d72da317cc551406f2d4f1d64991e18d40597c38
tanviranindo <tanviranindo@gmail.com>
2021-05-11 21:19:14 +0600

    Lab-06 Updated

--- Lab-06/Banker.java
+++ Lab-06/Banker.java
@@ -1,109 +1,122 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * @Name: Tanvir Rahman
+ * @StudentID: 19101268
+ */
 
 public class Banker {
-    static int n, m;
-    static int[] safe = new int[n + 10];
+    public static int processes, resources;
+    public static int[][] need;
+    public static int[] safeSequence = new int[processes + 5];
 
-    public static boolean safety(int[] available, int[][] allocation, int[][] need, int n, int m, int[][] changeInAvailable) {
+    public static void needMatrix(int[][] max, int[][] allocation) {
+        need = new int[processes][resources];
+        for (int i = 0; i < processes; i++) {
+            for (int j = 0; j < resources; j++) {
+                need[i][j] = max[i][j] - allocation[i][j];
+            }
+        }
+    }
 
-        boolean[] checkSafe = new boolean[n];
+    public static boolean isSafe(int[] available, int[][] allocation, int[][] changedAvailable) {
 
-        for (int i = 0; i < n; i++) checkSafe[i] = false;
+        boolean[] safeCheck = new boolean[processes];
 
-        int check = 0;
-        int check1 = 0;
+        int countOne = 0;
+        int countTwo = 0;
         do {
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < processes; i++) {
                 boolean flag = true;
-                if (!checkSafe[i]) {
-                    for (int j = 0; j < m; j++) {
+                if (!safeCheck[i]) {
+                    for (int j = 0; j < resources; j++) {
                         if (available[j] < need[i][j]) {
                             flag = false;
                             break;
                         }
                     }
                     if (flag) {
-                        for (int j = 0; j < m; j++) {
-                            available[j] = available[j] + allocation[i][j];
-                            changeInAvailable[check][j] = available[j];
+                        for (int j = 0; j < resources; j++) {
+                            available[j] += allocation[i][j];
+                            changedAvailable[countOne][j] = available[j];
                         }
-                        safe[check] = i;
-                        check++;
-                        checkSafe[i] = true;
+                        safeSequence[countOne] = i;
+                        countOne++;
+                        safeCheck[i] = true;
                     }
                 }
             }
-            check1++;
-        } while (check < n && check1 < n);
-        return check <= n;
+            countTwo++;
+        } while (countOne < processes && countTwo < processes);
+        return countOne <= processes;
     }
 
     public static void main(String[] args) throws IOException {
 
         File file = new File("C:\\Users\\terabaka\\IdeaProjects\\CSE321\\Lab-06\\input.txt");
-        Scanner input = new Scanner(file);
+        BufferedReader input = new BufferedReader(new FileReader(file));
+        StringTokenizer st;
 
-        // System.out.println("Enter no. of processes");
-        n = input.nextInt();
-        // System.out.println("Enter no. of resources:");
-        m = input.nextInt();
+        processes = Integer.parseInt(input.readLine());
+        resources = Integer.parseInt(input.readLine());
 
-        // System.out.println("Enter maximum matrix:");
-        int[][] max = new int[n][m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
-                // System.out.println("Enter maximum instances of resource " + j + " for process
-                // " + i);
-                max[i][j] = input.nextInt();
+        input.readLine();
+        int[][] max = new int[processes][resources];
 
+        for (int i = 0; i < processes; i++) {
+            st = new StringTokenizer(input.readLine());
+            for (int j = 0; j < resources; j++) {
+                max[i][j] = Integer.parseInt(st.nextToken());
             }
+        }
 
-        // System.out.println("Enter allocation matrix:");
-        int[][] allocation = new int[n][m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
-                // System.out.println("Enter allocated instances of resource " + j + " for
-                // process " + i);
-                allocation[i][j] = input.nextInt();
+        input.readLine();
+        int[][] allocation = new int[processes][resources];
 
+        for (int i = 0; i < processes; i++) {
+            st = new StringTokenizer(input.readLine());
+            for (int j = 0; j < resources; j++) {
+                allocation[i][j] = Integer.parseInt(st.nextToken());
             }
+        }
 
-        int[] available = new int[m];
-        for (int i = 0; i < m; i++) {
-            // System.out.println("Enter no. of available instances of resource " + i);
-            available[i] = input.nextInt();
+        input.readLine();
+        int[] available = new int[resources];
+
+        st = new StringTokenizer(input.readLine());
+        for (int i = 0; i < resources; i++) {
+            available[i] = Integer.parseInt(st.nextToken());
         }
 
         input.close();
 
-        int[][] need = new int[n][m];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
-                need[i][j] = max[i][j] - allocation[i][j];
-            }
+        needMatrix(max, allocation);
 
-        int[][] changeInAvailable = new int[n][m];
-        System.out.println("Need matrix is: ");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(need[i][j] + " ");
+        System.out.println("Need Matrix :");
+        for (int i = 0; i < processes; i++) {
+            for (int j = 0; j < resources; j++) {
+                System.out.print(need[i][j] + (j == resources - 1 ? "\n" : " "));
             }
-            System.out.println();
         }
 
-        if (safety(available, allocation, need, n, m, changeInAvailable)) {
+        int[][] changedAvailable = new int[processes][resources];
+
+        if (isSafe(available, allocation, changedAvailable)) {
             System.out.println("\nSafe sequence is :");
-            for (int i = 0; i < n; i++) {
-                System.out.print((char) ('A' + safe[i]) + " ");
+            for (int i = 0; i < processes; i++) {
+                System.out.print((char) ('A' + safeSequence[i]) + (i == processes - 1 ? "" : " "));
             }
-            System.out.println();
-            System.out.println("Change in available resource matrix : ");
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    System.out.print(changeInAvailable[i][j] + " ");
+
+            System.out.println("\nChange in available resource matrix :");
+
+            for (int i = 0; i < processes; i++) {
+                for (int j = 0; j < resources; j++) {
+                    System.out.print(changedAvailable[i][j] + (j == resources - 1 ? "\n" : " "));
                 }
-                System.out.println();
             }
         } else System.out.println("\nSystem is in unsafe state");
     }
